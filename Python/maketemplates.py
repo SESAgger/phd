1967268490f96601fa0bda090e0a4a19c6b9558a
SESAgger <sophieagger@gmail.com>
2021-07-23 12:40:01 +0200

    Added Maketemplate (again)

--- Python/maketemplates.py
+++ Python/maketemplates.py
@@ -6,8 +6,9 @@ import os
 import sys
 
 parser=argparse.ArgumentParser(description="Makes sbatch files based on a sample file (-s) and a template file (-t)")
-parser.add_argument("-s","--sample_file",help="sample file, can be in any format",default="samples.txt")
-parser.add_argument("-t","--template_file",help="template file, can be in any format",default="sbatch.txt")
+parser.add_argument("-s","--sample_file",help="sample file, can be in any format, path can be included (/proj/../fil.txt) ",default="samples.txt")
+parser.add_argument("-t","--template_file",
+help="template file, can be in any format. \n SAMPLE will be filled with what's in the sample file between the last '/' and the first '.' (fil).\n PATH will be changed to the full path found in the sample file (/proj/../fil.txt)",default="sbatch.txt")
 parser.add_argument("-n","--name", help="Name for produced files.",default="ny")
 argcomplete.autocomplete(parser)
 
@@ -22,21 +23,25 @@ filer=[]
 lfil=open(args.sample_file,'r')
 liste=lfil.readlines()
 
+#print(liste)
 tfil=open(args.template_file)
 template=tfil.readlines()
-
+#print(template)
 while i < len(liste):
 	sti=liste[i].replace('\n','')
 	i+=1
-	isample=str(sti).find('FIL1')
+	isample=str(sti).find('STI')
 	navn=str(sti)[isample+1:]
-	filnavn=navn+'_'+str(args.name)+'.sh'
+	sample=navn.split("/")[-1].split(".")[0]
+	filnavn=args.name+"_"+sample+'.sh'
+	#print(filnavn)
 	pth = ''
 	#print(navn+"done")
 	nyfil=open(filnavn,"w")
 	for j in range(len(template)):
-		nyline=template[j].replace('FIL1',navn)
+		nyline=template[j].replace('STI',navn)
 		nyline=nyline.replace('TID','5:0:0')
+		nyline=nyline.replace('SAMPLE',sample)
 		nyfil.write(nyline)
 		j+=1
 		#print(nyline)
@@ -50,7 +55,7 @@ w.write('''#!/bin/bash -l
 print("Made:")
 while k<len(filer):
 	if str(args.name) in filer[k]:
-		w.write('sbatch ' + filer[k] + '\n \n')
+		w.write('sbatch ' + filer[k] + '\n')
 		print(filer[k])
 	k+=1
 w.close()
@@ -58,3 +63,4 @@ w.close()
 print("\n"+str(args.sample_file) +" is used as the sample file\n"
 + str(args.template_file) +" is used as the template file\n"
 +"files will be named sample_name_"+str(args.name)+".sh")
+
