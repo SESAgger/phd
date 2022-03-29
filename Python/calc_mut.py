585e3480a83476b75a4e794a145f46b75fb908b6
SESAgger <sophieagger@gmail.com>
2022-03-29 16:52:52 +0200

    Find AA with consequence, calc_mut added

---
+++ Python/calc_mut.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+
+import argparse
+import os
+import time
+import sys
+import csv
+import pandas as pd 
+import numpy as np
+
+from sigfig import round
+
+
+parser=argparse.ArgumentParser(description="Calculates mutational load")
+parser.add_argument("-s","--sample_file",help="tsv with CHROM, POS, TGT")
+parser.add_argument("-r","--reference_file",
+help="What's your reference")
+parser.add_argument("-n","--name", help="Name for produced files.",default="ny")
+#argcomplete.autocomplete(parser)
+
+args = parser.parse_args()
+
+
+
+#Import the specific canids genotype info
+canid = pd.read_csv("$SCRATCH/$line.tsv", sep = '\t', names = ["CHROM","POS","TGT"])
+name = $line
+
+#Import the reference file
+refa = pd.read_csv($reffile, sep='\t',dtype = {'CHROM': object, 'POS': int, 'AA': object, 'DER': object, 'Type': object, 'PhyloP': float, 'SIFT_txt': object, 'SIFT_score': float, 'Consequence': object })
+
+# Combine the 2 dataframes
+canid_for_calc=canid.merge(refa, how = "left")
+
+# Figure out if the GT is homozygous derived allel or if either PhyloP, sift or derived allele is unknown 
+der_and_tv = (canid_for_calc["TGT"] == canid_for_calc["DER"] + "/" + canid_for_calc["DER"])
+
+## Across genome
+phylo_score_hom_tv = canid_for_calc[der_and_tv].PhyloP.sum()
+sift_score_hom_tv = canid_for_calc[der_and_tv].SIFT_score.sum()
+#phastcon_score_hom_tv = canid_for_calc[der_and_tv].PhastCon.sum()
+
+## Genic
+phylo_score_hom_tv_genic = canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]!="intergenic_variant")].PhyloP.sum()
+sift_score_hom_tv_genic = canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]!="intergenic_variant")].SIFT_score.sum()
+#phastcon_score_hom_tv = canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]!="intergenic_variant")].PhastCon.sum()
+
+## Non-genic
+phylo_score_hom_tv_nongenic = canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]=="intergenic_variant")].PhyloP.sum()
+sift_score_hom_tv_nongenic = canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]=="intergenic_variant")].SIFT_score.sum()
+#phastcon_score_hom_tv = canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]=="intergenic_variant")].PhastCon.sum()
+
+# Generals
+
+## Genic and nongenic SNPs
+intergenic_variants=len(canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]=="intergenic_variant")])
+genic_variants=len(canid_for_calc[(der_and_tv)&(canid_for_calc["Consequence"]!="intergenic_variant")])
+
+
+## Number of locations where a transversion has happened and gt=the derived allele
+hom_der=len(canid_for_calc[(canid_for_calc["Type"]=="V")&(canid_for_calc["TGT"] == canid_for_calc["DER"] + "/" + canid_for_calc["DER"])].index)
+hom_der_genic=len(canid_for_calc[(canid_for_calc["Type"]=="V")&(canid_for_calc["TGT"] == canid_for_calc["DER"] + "/" + canid_for_calc["DER"])&(canid_for_calc["Consequence"]=="intergenic_variant")].index)
+hom_der_nongenic=len(canid_for_calc[(canid_for_calc["Type"]=="V")&(canid_for_calc["TGT"] == canid_for_calc["DER"] + "/" + canid_for_calc["DER"])&(canid_for_calc["Consequence"]!="intergenic_variant")].index)
+
+## Number of locations where gt=the ancestral allele
+hom_anc=len(canid_for_calc[(canid_for_calc["Type"]=="V")&(canid_for_calc["TGT"] == canid_for_calc["AA"] + "/" + canid_for_calc["AA"])].index)
+hom_anc_genic=len(canid_for_calc[(canid_for_calc["Type"]=="V")&(canid_for_calc["TGT"] == canid_for_calc["AA"] + "/" + canid_for_calc["AA"])&(canid_for_calc["Consequence"]=="intergenic_variant")].index)
+hom_anc_nongenic=len(canid_for_calc[(canid_for_calc["Type"]=="V")&(canid_for_calc["TGT"] == canid_for_calc["AA"] + "/" + canid_for_calc["AA"])&(canid_for_calc["Consequence"]!="intergenic_variant")].index)
+
+## Results
+### All
+phylop_mutational_load=phylo_score_score_hom_tv/(hom_der+hom_anc)
+sift_mutational_load=sift_score_score_hom_tv/(hom_der+hom_anc)
+
+### Genic
+phylop_mutational_load_genic=phylo_score_hom_tv_genic/(hom_der_genic+hom_anc_genic)
+sift_mutational_load_genic=sift_score_hom_tv_genic/(hom_der+hom_anc)
+
+### Non-genic
+phylop_mutational_load_nongenic=phylo_score_hom_tv_nongenic/(hom_der_nongenic+hom_anc_nongenic)
+sift_mutational_load_nongenic=sift_score_hom_tv_nongenic/(hom_der_nongenic+hom_anc_nongenic)
+
+#Summary
+w=open(name+"mutational_load_summary.txt",'w')
+w.write(
+f'''#Summary from processing of test
+#Can be read as tsv with comment="#" and sep="\\t"
+## Background info
+Number of non-genic variants:\t {intergenic_variants} 
+Number of non-genic variants:\t {genic_variants} 
+
+Number of derived alleles:\t {hom_der}
+Number of genic derived alleles:\t {hom_der_genic}
+Number of nongenic derived alleles:\t {hom_der_nongenic}
+Number of ancestral alleles:\t {hom_anc}
+Number of genic ancestral alleles:\t {hom_anc_genic}
+Number of nongenic ancestral alleles:\t {hom_anc_genic}
+
+## Results
+### All positions
+PhyloP mutational load:\t  {round(phylop_mutational_load,sigfigs=2)}  \n
+Sift mutational load:\t  {round(sift_mutational_load,sigfigs=2)}  \n
+    
+### Genic positions
+Genic PhyloP mutational load:\t  {round(phylop_mutational_load_genic,sigfigs=2)}
+Genic Sift mutational load:\t  {round(sift_mutational_load_genic,sigfigs=2)}
+    
+### Nongenic positions
+Nongenic PhyloP mutational load:\t  {round(phylop_mutational_load_nongenic,sigfigs=2)}
+Nongenic Sift mutational load:\t  {round(sift_mutational_load_nongenic,sigfigs=2)}
+'''
+)
+w.close()
+
--- Python/findAA.py
+++ Python/findAA.py
@@ -8,52 +8,92 @@ import sys
 import csv
 import pandas as pd 
 import numpy as np
+from sigfig import round
 
 parser=argparse.ArgumentParser(description="Finds AA and TSTV for all positions.")
-parser.add_argument("-s","--sample_file",help="sample file, can be in any format, path can be included (/proj/../fil.txt) ",default="samples.txt")
+parser.add_argument("-s","--sample_file",help="sample file, must be tsv, path can be included (/proj/../fil.txt) ",default="samples.tsv")
 parser.add_argument("-n","--name",help="What you want the output to be called", default="AA")
+parser.add_argument("-p","--phylop",help="phylopfile")
+parser.add_argument("-s","--sift",help="siftfile")
+
 
 args = parser.parse_args()
-df=pd.read_csv(args.sample_file,sep=',')
+aa = pd.read_csv(args.sample_file,sep='\t')
+number_of_canids=len(aa.columns[aa.columns.str.find("GT")!=-1])
 
-canids=df.columns[4:29]
+aa.columns=aa.columns.str.lstrip(" # [1234567890]").str.replace(":GT","")
 
-df["homref"]=df[df[canids]=="0/0"].count(axis=1)
-df["het"]=df[df[canids]=="0/1"].count(axis=1)
-df["homalt"]=df[df[canids]=="1/1"].count(axis=1)
+#Define columns containing canids 
+canids=aa.columns[4:4+number_of_canids]
 
+#Count number of each category. Could instead count the ones that aren't the 3, but I think this is fine
+aa["homref"] = aa[aa[canids] == "0/0"].count(axis = 1)
+aa["het"]    = aa[aa[canids] == "0/1"].count(axis = 1)
+aa["homalt"] = aa[aa[canids] == "1/1"].count(axis = 1)
 
+#Infer the ancestral allele
+#If the dhole is {REF|ALT} and either Adu or Mes is {REF|ALT} and at least half of the rest of the canids are {REF|ALT} the {REF|ALT} is the ancestral allele
+#half is 13: 24 canids in total, we know at least 2 are {REF|ALT} (first two requirements), so half of the rest is 11 (22/2) and then we add the 2 back=13
 conditions = [
-    (df.BerlinZoo=="0/0")&(df.C_adustus=="0/0")|(df.C_mesomelas=="0/0")&(df.homref >=10),
-    (df.BerlinZoo=="1/1")&(df.C_adustus=="1/1")|(df.C_mesomelas=="1/1")&(df.homalt >=10),
+    (aa.BerlinZoo == "0/0") & 
+        ((aa.C_adustus == "0/0") | (aa.C_mesomelas == "0/0")) & 
+        (aa.homref >= (number_of_canids-2)/2+2),
+    (aa.BerlinZoo == "1/1") & 
+        ((aa.C_adustus == "1/1") | (aa.C_mesomelas == "1/1")) & 
+        (aa.homalt >= (number_of_canids-2)/2+2),
 ]
-values = [
-    df.REF,df.ALT
+#if above is REF, then anc=ref and if above is alt, then anc=alt 
+anc_all = [
+    aa.REF, aa.ALT
+]
+#if above is REF, then der=alt and if above is alt, then der=ref
+der_all = [
+    aa.ALT, aa.REF
 ]
 
+# make the columns
+aa['AA']  = np.select(conditions, anc_all, default = np.nan)
+aa['DER'] = np.select(conditions, der_all, default = np.nan)
 
-df['AA']=np.select(conditions,values,default="NA")
-
-condlist=[
-    (((df['REF']=='A')|(df['REF']=='G'))&
-        ((df['ALT']=='C')|(df['ALT']=='T'))
+#Determine if it is a transversion or not
+tstv = [
+    (((aa['REF']=='A')|(aa['REF']=='G'))&
+        ((aa['ALT']=='C')|(aa['ALT']=='T'))
     )|
     (
-        ((df['ALT']=='A')|(df['ALT']=='G'))&
-        ((df['REF']=='C')|(df['REF']=='T'))
+        ((aa['ALT']=='A')|(aa['ALT']=='G'))&
+        ((aa['REF']=='C')|(aa['REF']=='T'))
     ),
     (
-        ((df['REF']=='A')|(df['REF']=='G'))&
-        ((df['ALT']=='A')|(df['ALT']=='G'))
+        ((aa['REF']=='A')|(aa['REF']=='G'))&
+        ((aa['ALT']=='A')|(aa['ALT']=='G'))
     )|
     (
-        ((df['ALT']=='C')|(df['ALT']=='T'))&
-        ((df['REF']=='C')|(df['REF']=='T'))
+        ((aa['ALT']=='C')|(aa['ALT']=='T'))&
+        ((aa['REF']=='C')|(aa['REF']=='T'))
     )
 ]
 
-df['Type'] = np.select(condlist,choicelist=['V','S'],default="NA")
+#Make the column
+aa['Type'] = np.select(tstv,choicelist = ['V','S'],default=np.nan)
 
-name=args.name+"_annotated_AA_TSTV.tsv"
+#Import the PhyloP scores
+phylop = pd.read_csv(args.phylop, sep = '\t', usecols = [0,2,4], names = ['CHROM','POS','PhyloP'], header = 0)
 
-df.to_csv(name,sep="\t",columns=["#CHROM","POS","AA","Type"],index=False)
+#Make reference file (should also contain phastcon and sift at some point)
+ref = aa.merge(phylop, how = "left")
+
+sift = pd.read_csv(args.sift,sep="\t",comment="#",usecols=[1,6,13],names=['Location','Consequence','Extra'])
+
+sift[['CHROM','POS']]=sift.Location.str.split(":",expand=True)
+sift['POS']=sift.POS.astype(int)
+sift['SIFT_txt']=sift.Extra.str.extract(r'SIFT=(\w+)')
+sift['SIFT_score']=sift.Extra.str.extract(r'SIFT=.*(\d+\.\d*)').astype(float)
+sift.drop(columns=["Extra","Location"])
+
+ref1=ref.merge(sift,how="outer")
+
+#Make reference file
+ref1.to_csv(args.name+"_annotated_AA_TSTV_PhyloP_SIFT.tsv",sep="\t",columns=["CHROM","POS","AA","DER","Type","PhyloP","SIFT_score","SIFT_txt","Consequence"],index=False)
+
+ref1[(ref1["AA"]!="")&(ref1["Type"]=="V")&(ref1.SIFT_txt.notna())].to_csv(args.name+"_der_transversions_sift_pp.tsv",sep="\t",columns=["CHROM","POS","DER","Type","PhyloP","SIFT_score","SIFT_txt","Consequence"],index=False)
