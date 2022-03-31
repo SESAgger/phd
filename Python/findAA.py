793dc5afa49c946fde6a4036f93f740d369ed05f
SESAgger <sophieagger@gmail.com>
2022-03-31 20:42:27 +0200

    findAA updated with phastcon

--- Python/findAA.py
+++ Python/findAA.py
@@ -15,10 +15,27 @@ parser.add_argument("-i","--input_file",help="sample file, must be tsv, path can
 parser.add_argument("-n","--name",help="What you want the output to be called", default="AA")
 parser.add_argument("-p","--phylop",help="phylopfile")
 parser.add_argument("-s","--sift",help="siftfile")
+parser.add_argument("-h","--phast",help="phastfile")
+
 
 
 args = parser.parse_args()
+# Import files
+## Import the gts
 aa = pd.read_csv(args.input_file,sep='\t')
+## Import the PhyloP scores
+phylop = pd.read_csv(args.phylop, sep = '\t', usecols = [0,2,4], names = ['CHROM','POS','PhyloP'], header = 0)
+## Import the Sift scores
+sift = pd.read_csv(args.sift,sep="\t",comment="#",usecols=[1,6,13],names=['Location','Consequence','Extra'])
+sift[['CHROM','POS']]=sift.Location.str.split(":",expand=True)
+sift['POS']=sift.POS.astype(int)
+sift['SIFT_txt']=sift.Extra.str.extract(r'SIFT=(\w+)')
+sift['SIFT_score']=sift.Extra.str.extract(r'SIFT=.*(\d+\.\d*)').astype(float)
+sift.drop(columns=["Extra","Location"])
+
+## Import the PhastCon scores
+phast=pd.read_csv(args.phast,sep="\t",names=["CHROM","start","POS","PhastCon"])
+
 number_of_canids=len(aa.columns[aa.columns.str.find("GT")!=-1])
 
 aa.columns=aa.columns.str.lstrip(" # [1234567890]").str.replace(":GT","")
@@ -77,25 +94,14 @@ tstv = [
 #Make the column
 aa['Type'] = np.select(tstv,choicelist = ['V','S'],default=np.nan)
 
-#Import the PhyloP scores
-phylop = pd.read_csv(args.phylop, sep = '\t', usecols = [0,2,4], names = ['CHROM','POS','PhyloP'], header = 0)
-
-#Make reference file (should also contain phastcon and sift at some point)
+#Make reference file
 ref = aa.merge(phylop, how = "left")
-
-sift = pd.read_csv(args.sift,sep="\t",comment="#",usecols=[1,6,13],names=['Location','Consequence','Extra'])
-
-sift[['CHROM','POS']]=sift.Location.str.split(":",expand=True)
-sift['POS']=sift.POS.astype(int)
-sift['SIFT_txt']=sift.Extra.str.extract(r'SIFT=(\w+)')
-sift['SIFT_score']=sift.Extra.str.extract(r'SIFT=.*(\d+\.\d*)').astype(float)
-sift.drop(columns=["Extra","Location"])
 
 ref1=ref.merge(sift,how="outer")
 
+ref2=ref1.merge(phast,how="left")
+
 #Make reference file
-ref1.to_csv(args.name+"_annotated_AA_TSTV_PhyloP_SIFT.tsv",sep="\t",columns=["CHROM","POS","AA","DER","Type","PhyloP","SIFT_score","SIFT_txt","Consequence"],index=False)
+ref2.to_csv("~/Downloads/ref.tsv", sep = "\t", columns = ["CHROM","POS","AA","DER","Type","PhyloP","SIFT_txt","SIFT_score","Consequence","PhastCon"], index = False)
 
-ref1[(ref1["AA"]!="")&(ref1["Type"]=="V")&(ref1.SIFT_txt.notna())].to_csv(args.name+"_der_transversions_sift_pp.tsv",sep="\t",columns=["CHROM","POS","AA","DER","Type","PhyloP","SIFT_score","SIFT_txt","Consequence"],index=False)
-
-print("Files called: "args.name+"_annotated_AA_TSTV_PhyloP_SIFT.tsv and " + args.name+"_der_transversions_sift_pp.tsv")
+print("File is called: "args.name+"_annotated_AA_TSTV_PhyloP_SIFT.tsv")
